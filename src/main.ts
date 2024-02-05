3d66e1c3f57be98874c0d0712c2abba3b7390b75
Anton Makiievskyi <iammckiev@gmail.com>
2024-02-05 15:34:51 -0800

    refactor: moved most railgun functions into separate module

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,7 @@ import * as Railgun from './railgun/railgun'
 import config from './config';
 
 async function main() {
-    Railgun.initializeEngine();
-    await Railgun.loadEngineProvider();
-    Railgun.setEngineLoggers();
-    
-    const railgunWalletInfo = await Railgun.createWallet(config.encryptionKey, config.mnemonic, Railgun.creationBlockNumberMap);
-    console.log(railgunWalletInfo);
-    const wallet = Railgun.walletForID(railgunWalletInfo.id); // Store this value.
-
-    await wallet.getTokenBalances(TXIDVersion.V2_PoseidonMerkle, Railgun.chain, false); // onlySpendable
-  
-    const tx_info: Railgun.TxInfo = { length: 0 };
-    Railgun.setOnBalanceUpdateCallback((balancesFormatted) => Railgun.onBalanceUpdateCallback(balancesFormatted, wallet, Railgun.chain, tx_info));
-    //setOnUTXOMerkletreeScanCallback(Railgun.onMerkletreeScanCallback);
-    
-    await Railgun.refreshBalances(Railgun.chain, undefined);
-    console.log('refreshBalances done');
+    const wallet = await Railgun.start();
   }  
 
 main().catch(console.error);
