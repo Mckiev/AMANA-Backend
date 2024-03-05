ba51dba17bedcdcb273b988eac426e21506ef8ec
Anton Makiievskyi <iammckiev@gmail.com>
2024-03-04 20:00:22 -0800

    added logs for debugging

--- src/betProcessor.ts
+++ src/betProcessor.ts
@@ -48,14 +48,16 @@ const processRedemption = async (): Promise<void> => {
     const [received_mana, bet_array] = await Manifold.closePosition(redemption.marketId, redemption.prediction, Number(redemption.nShares));
     if (received_mana) {
       for (const bet of bet_array) {
-        database.createRedemptionTransaction(redemption.id, bet);
+        await database.createRedemptionTransaction(redemption.id, bet);
       }
       // Using deposit function to process redemption
       const railgunAddress = redemption.redemptionAddress;
       const manifoldUserId = await Manifold.fetchMyId();
       const manifoldTransferId = 'undefined';
-      database.createDeposit(railgunAddress, manifoldTransferId, manifoldUserId, BigInt(received_mana));
-      database.updateBetToRedeemed(redemption.id);
+
+      await database.createDeposit(railgunAddress, manifoldTransferId, manifoldUserId, BigInt(received_mana));
+      console.log('Redemption processed');
+      await database.updateBetToRedeemed(redemption.id);
     } else {
       throw new Error('Failed to redeem');
     }
