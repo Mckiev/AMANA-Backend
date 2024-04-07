245d472a425f214b315ee75b26277c1846a9bcc2
Anton Makiievskyi <iammckiev@gmail.com>
2024-04-07 15:39:47 -0700

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import * as dotenv from 'dotenv';
-import constants from './constants';
 dotenv.config();
 
 // TODO will need to generate this safely in the future
@@ -21,7 +20,6 @@ const apiKey = process.env.MANIFOLD_BOT_API_KEY ?? '';
 const mckievAPIKey = process.env.MANIFOLD_MCKIEV_API_KEY ?? '';
 const polygonApiKey = process.env.POLYGON_INFURA_API ?? '';
 const rpc1_api = process.env.RPC1_API ?? '';
-
 
 export default {
   mnemonic,
